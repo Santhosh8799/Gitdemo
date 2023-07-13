bd08285f43167cb883f8c0f5d66f3cdfbdf5fdf1
Santhosh8799 <santhosharies32@gmail.com>
2023-07-13 15:22:48 +0530

    Asian team Development

---
+++ src/RestassuredGit.java
@@ -0,0 +1,13 @@
+
+public class RestassuredGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Mobile automation frameowrk");
+		System.out.println("Web automation frameowrk");
+		System.out.println("Desktop automation frameowrk");
+	}
+
+}
